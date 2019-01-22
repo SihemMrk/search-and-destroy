57633591b0a4f2854d6a673f49ddcff74e5096e5
Sihem Meriki <sihemmeriki2@gmail.com>
2019-01-22 17:36:53 -0500

    jump

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,16 @@
 
 // Complete this algo
 const minJumps = arr => {
+    let firstEl = arr[0];
+    if(firstEl >= arr.length - 1){
+        return 1
+    }else{
 
+        let newArr = arr.slice(1, firstEl + 1);
+        let max = Math.max(...newArr);
+        let arr3 = arr.splice(arr.indexOf(max));
+        return 1 + minJumps(arr3)
+    }
 };
 
 module.exports = minJumps
